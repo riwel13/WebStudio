c9c6310931d0759a2d74ca3c5193d8f87725e192
riwel13 <karina.velmushkina@gmail.com>
2025-07-29 20:30:23 +0300

    yayayayaya

--- modal.js
+++ modal.js
@@ -9,3 +9,16 @@ closeModalBtn.addEventListener('click', toggleModal);
 function toggleModal() {
   backdrop.classList.toggle('is-hidden');
 }
+
+const oopenModalBtn = document.querySelector('.burger-open');
+const clooseModalBtn = document.querySelector('.burger-close');
+const burger = document.querySelector('.burger_window');
+
+oopenModalBtn.addEventListener('click', toggleModal);
+
+clooseModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  burger.classList.toggle('is-hiddenn');
+}
+
